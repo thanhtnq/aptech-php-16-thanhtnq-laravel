3f43d2b3683c92828f2a9f4c76dc85d8fb721126
thanhtnq <46392329+thanhtnq@users.noreply.github.com>
2019-01-08 23:16:22 +0700

    Add files via upload

---
+++ php/3-2019-01-08/dangnhap.php
@@ -0,0 +1,6 @@
+<?php
+    if ($_GET['thu-dien-tu'] === 'a@gmail.com' && $_GET['mat-khau'] === '123456') {
+        echo "Dang nhap thanh cong!";
+    } else {
+        echo "Dang nhap that bai!";
+    }
---
+++ php/3-2019-01-08/thanhtnq.php
@@ -0,0 +1,13 @@
+<form action="dangnhap.php" method="GET">
+    <div>
+        <label for="email">Emalil</label>
+        <input type="email" name="thu-dien-tu" required />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" name="mat-khau" required />
+    </div>
+    <div>
+        <button type="submit">Login</button>
+    </div>
+</form>
